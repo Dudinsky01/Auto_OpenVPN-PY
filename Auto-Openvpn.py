1ff69985868f2d9407cbf52bc19aaf2c66175463
Dudinsky <24269422+Dudinsky01@users.noreply.github.com>
2018-11-13 19:45:51 +0100

    Update Auto-Openvpn.py

--- Auto-Openvpn.py
+++ Auto-Openvpn.py
@@ -1,44 +1,64 @@
-#!/usr/bin/env python
-
 import os
-import socket
+import sys
+import tarfile
+import subprocess
 from OpenSSL import crypto, SSL
 
+#   Change this if you want to use a different size for DH_PARAMS
+DH_SIZE = 2048
+
+#   Create four dict with the values needed to create CA and certs
+def config_creator():
+    server_ca = {}
+    server_cert = {}
+    client_ca = {}
+    client_cert = {}
+
+#   Server Certification Authority
+    server_ca['commonName'] = "Server CA"
+    server_ca['cert_filename'] = "server_ca.pem"
+    server_ca['cert_key'] = "server_ca.key"
+    server_ca['serial'] = 12345999
+    server_ca['validfrom'] = "20180101000000Z"
+    server_ca['validto'] = "20200101000000Z"
+    server_ca['keyfilesize'] = 4096
+    server_ca['hashalgorithm'] = "sha512"
+
+#   Server Certificate (signed by the CA above)
+    server_cert['commonName'] = "Server Cert"
+    server_cert['cert_filename'] = "server_cert.pem"
+    server_cert['cert_key'] = "server_cert.key"
+    server_cert['serial'] = 12345888
+    server_cert['validfrom'] = "20180101000000Z"
+    server_cert['validto'] = "20200101000000Z"
+    server_cert['keyfilesize'] = 4096
+    server_cert['hashalgorithm'] = "sha512"
+
+#   Client Certification Authority
+    client_ca['commonName'] = "Client CA"
+    client_ca['cert_filename'] = "client_ca.pem"
+    client_ca['cert_key'] = "client_ca.key"
+    client_ca['serial'] = 12345777
+    client_ca['validfrom'] = "20180101000000Z"
+    client_ca['validto'] = "20200101000000Z"
+    client_ca['keyfilesize'] = 4096
+    client_ca['hashalgorithm'] = "sha512"
+
+#   Client Certificate (signed by the CA above)
+    client_cert['commonName'] = "Client Cert"
+    client_cert['cert_filename'] = "client_cert.pem"
+    client_cert['cert_key'] = "client_cert.key"
+    client_cert['serial'] = 12345666
+    client_cert['validfrom'] = "20180101000000Z"
+    client_cert['validto'] = "20200101000000Z"
+    client_cert['keyfilesize'] = 4096
+    client_cert['hashalgorithm'] = "sha512"
+
+    return server_ca, server_cert, client_ca, client_cert
 
 
-# Crée une clé RSA avec le nombre de bits spécifié.
-
-# create an RSA key with the specified number of bits.
-def create_key(algorithm=crypto.TYPE_RSA, numbits=2048):
-    pkey = crypto.PKey()
-    pkey.generate_key(algorithm, numbits)
-    return pkey
-
-# Crée un certificat et une demande de signature de celui-ci avec les attributs spécifiés ci-dessous
-# Changez les attributs pour correspondre à vos besoin.
-
-# Creates a certificate signing request with the specified subject attributes.
-# Change the specified subject attributes to fit your needs.
-def create_cert(pkey, CN, C=None, ST=None, L=None, O=None, OU=None, EmailAddress=None, hashalgorithm='sha256WithRSAEncryption'):
-    req = crypto.X509Req()
-    req.get_subject()
-    subj = req.get_subject()
-
-    if C:
-        subj.C = C
-    if ST:
-        subj.ST = ST
-    if L:
-        subj.L = L
-    if O:
-        subj.O = O
-    if OU:
-        subj.OU = OU
-    if CN:
-        subj.CN = CN
-    if EmailAddress:
-        subj.emailAddress = EmailAddress
-
-    req.set_pubkey(pkey)
-    req.sign(pkey, hashalgorithm)
-    return req
+#   generates a key for the CA and Certs of generate_ca() and generate_certificate()
+def generate_key(size):
+    key = crypto.PKey()
+    key.generate_key(crypto.TYPE_RSA, size)
+    return key
