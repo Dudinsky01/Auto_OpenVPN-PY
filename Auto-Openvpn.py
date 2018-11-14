d52d14c4bf068a038b68c8cbb2926dea4778b140
Dudinsky <24269422+Dudinsky01@users.noreply.github.com>
2018-11-14 15:23:18 +0100

    Update Auto-Openvpn.py

--- Auto-Openvpn.py
+++ Auto-Openvpn.py
@@ -58,7 +58,112 @@ def config_creator():
 
 
 #   generates a key for the CA and Certs of generate_ca() and generate_certificate()
-def generate_key(size):
+def create_key(size):
     key = crypto.PKey()
     key.generate_key(crypto.TYPE_RSA, size)
     return key
+
+#   Generates a CA certificate
+def generate_ca(ca_dict):
+    ca = crypto.X509()
+    ca.set_version(2)
+    ca.set_serial_number(ca_dict['serial'])
+    ca_subj = ca.get_subject()
+    if 'commonName' in ca_dict:
+        ca_subj.commonName = ca_dict['commonName']
+    if 'stateOrProvinceName' in ca_dict:
+        ca_subj.stateOrProvinceName = ca_dict['stateOrProvinceName']
+    if 'localityName' in ca_dict:
+        ca_subj.localityName = ca_dict['localityName']
+    if 'organizationName' in ca_dict:
+        ca_subj.organizationName = ca_dict['organizationName']
+    if 'organizationalUnitName' in ca_dict:
+        ca_subj.organizationalUnitName = ca_dict['organizationalUnitName']
+    if 'emailAddress' in ca_dict:
+        ca_subj.emailAddress = ca_dict['emailAddress']
+    if 'countryName' in ca_dict:
+        ca_subj.countryName = ca_dict['countryName']
+    if 'validfrom' in ca_dict:
+        ca.set_notBefore(ca_dict['validfrom'])
+    if 'validto' in ca_dict:
+        ca.set_notAfter(ca_dict['validto'])
+    key = create_key(ca_dict['keyfilesize'])
+
+    ca.add_extensions([
+        crypto.X509Extension("basicConstraints", True, "CA:TRUE, pathlen:0"),
+        crypto.X509Extension("keyUsage", False, "keyCertSign, cRLSign"),
+        crypto.X509Extension("subjectKeyIdentifier",
+                             False, "hash", subject=ca),
+    ])
+
+    ca.add_extensions([
+        crypto.X509Extension("authorityKeyIdentifier",
+                             False, "keyid:always", issuer=ca)
+    ])
+
+    ca.set_issuer(ca.get_subject())
+    ca.set_pubkey(key)
+    ca.sign(key, ca_dict['hashalgorithm'])
+    return ca, key
+
+#   generates a Cert certificate
+def generate_certificate(certificate_dict, ca, cakey, name):
+
+#   Generate the private key
+    key = create_key(certificate_dict['keyfilesize'])
+
+#   Generate the certificate request
+    req = crypto.X509Req()
+    req_subj = req.get_subject()
+    if 'commonName' in certificate_dict:
+        req_subj.commonName = certificate_dict['commonName']
+    if 'stateOrProvinceName' in certificate_dict:
+        req_subj.stateOrProvinceName = certificate_dict['stateOrProvinceName']
+    if 'localityName' in certificate_dict:
+        req_subj.localityName = certificate_dict['localityName']
+    if 'organizationName' in certificate_dict:
+        req_subj.organizationName = certificate_dict['organizationName']
+    if 'organizationalUnitName' in certificate_dict:
+        req_subj.organizationalUnitName = certificate_dict['organizationalUnitName']
+    if 'emailAddress' in certificate_dict:
+        req_subj.emailAddress = certificate_dict['emailAddress']
+    if 'countryName' in certificate_dict:
+        req_subj.countryName = certificate_dict['countryName']
+
+    req.set_pubkey(key)
+    req.sign(key, certificate_dict['hashalgorithm'])
+
+#   Now generate the certificate itself
+    cert = crypto.X509()
+    cert.set_version(2)
+    cert.set_serial_number(certificate_dict['serial'])
+    cert.set_subject(req.get_subject())
+    cert.set_pubkey(req.get_pubkey())
+    cert.set_issuer(ca.get_subject())
+
+    if 'validfrom' in certificate_dict:
+        cert.set_notBefore(certificate_dict['validfrom'])
+    if 'validto' in certificate_dict:
+        cert.set_notAfter(certificate_dict['validto'])
+
+    if name == 'client':
+        usage = 'clientAuth'
+        nscerttype = 'client'
+    elif name == 'server':
+        usage = 'serverAuth'
+        nscerttype = 'server'
+
+    cert.add_extensions([
+        crypto.X509Extension("basicConstraints", True, "CA:FALSE"),
+        crypto.X509Extension(
+            "keyUsage", False, "digitalSignature,keyAgreement"),
+        crypto.X509Extension("extendedKeyUsage", False, usage),
+        crypto.X509Extension("nsCertType", False, nscerttype),
+        crypto.X509Extension("subjectKeyIdentifier",
+                             False, "hash", subject=cert),
+        crypto.X509Extension("authorityKeyIdentifier",
+                             False, "keyid:always", issuer=ca)
+    ])
+
+    cert.sign(cakey, certificate_dict['hashalgorithm'])
+    return req, cert, key
